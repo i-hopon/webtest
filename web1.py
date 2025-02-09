33bc7c49a129138080c1158d1a7fa5e85c680546
i-hopon <lcdbusiness@outlook.com>
2025-02-09 19:26:20 +0530

    Update web1.py

--- web1.py
+++ web1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title="dm",page_icon=":boom:")
 #header
-st.subheader("dmn")
-st.title("dm no")
-st.write("hop no")
+with st.container():
+  st.subheader("dmn")
+  st.title("dm no")
+  st.write("hop no")
