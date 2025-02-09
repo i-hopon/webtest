00a4b9567f2f782fb5fd6f2cf98d75027350064a
i-hopon <lcdbusiness@outlook.com>
2025-02-09 19:33:33 +0530

    Update web1.py

--- web1.py
+++ web1.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title="dm",page_icon=":boom:")
 #header
 with st.container():
-  st.image("https://tenor.com/view/red-alphabet-letter-dancing-letter-l-cartoons-gif-12084376")
   st.subheader("dmn")
   st.title("dm no")
+  st.image("https://tenor.com/view/red-alphabet-letter-dancing-letter-l-cartoons-gif-12084376",use_column_width=True)
   st.write("hop no")
