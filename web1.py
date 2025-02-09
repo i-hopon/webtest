e9b13fc28bb6f8e6811f358c35d873735aca0a01
i-hopon <lcdbusiness@outlook.com>
2025-02-09 19:33:59 +0530

    Update web1.py

--- web1.py
+++ web1.py
@@ -4,5 +4,5 @@ st.set_page_config(page_title="dm",page_icon=":boom:")
 with st.container():
   st.subheader("dmn")
   st.title("dm no")
-  st.image("https://tenor.com/view/red-alphabet-letter-dancing-letter-l-cartoons-gif-12084376",use_column_width=True)
+  st.image("https://tenor.com/view/red-alphabet-letter-dancing-letter-l-cartoons-gif-12084376",use_container_width=True)
   st.write("hop no")
